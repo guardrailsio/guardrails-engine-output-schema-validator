cb4542ae7861697c47b2c2f649131d74bc5de8c1
Stefan Streichsbier <stefan@streichsbier.at>
2019-06-05 15:07:45 +0100

    add languages

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const envelopeSchema = Joi.object().keys({
     })
     .required(),
   language: Joi.string()
-    .valid("javascript", "python", "ruby", "go", "solidity", "general", "php", "java", "dotnet")
+    .valid("javascript", "python", "ruby", "go", "solidity", "general", "php", "java", "dotnet", "elixir", "c")
     .required(),
   status: Joi.string()
     .valid("success", "failure")
